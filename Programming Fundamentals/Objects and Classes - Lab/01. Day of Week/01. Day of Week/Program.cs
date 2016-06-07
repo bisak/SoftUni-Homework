cd03f80daa0734a342f9aa5adb69db1b60994b19
Biser Atanasov <biser.p.atanasov@gmail.com>
2016-06-07 21:08:19 +0300

    Added empty projects to do tomorrow.

---
+++ Programming Fundamentals/Objects and Classes - Lab/01. Day of Week/01. Day of Week/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] date = Console.ReadLine().Split('-').Select(int.Parse).ToArray();
+            DateTime dates = new DateTime(date[2], date[1], date[0]);
+            Console.WriteLine(dates.DayOfWeek);
+        }
+    }
+}
