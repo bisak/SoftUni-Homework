ae5a6177de44e5a277afd64f42b5502c03ec77d5
Biser Atanasov <biser.p.atanasov@gmail.com>
2016-06-10 20:24:23 +0300

    update

--- Programming Fundamentals/Programming Fundamentals Sample Exam II - June 2016/02. SoftUni Water Supplies/02. SoftUni Water Supplies/Program.cs
+++ Programming Fundamentals/Programming Fundamentals Sample Exam II - June 2016/02. SoftUni Water Supplies/02. SoftUni Water Supplies/Program.cs
@@ -16,32 +16,66 @@ namespace _02.SoftUni_Water_Supplies
             decimal bottlesCapacity = decimal.Parse(Console.ReadLine());
 
             decimal consumedWater = 0;
+            decimal bottlesFilled = 0;
+            decimal bottlesLeft = 0;
 
+            decimal allBottles = bottlesArray.Length;
 
-            for (int i = 0; i < bottlesArray.Length; i++)
+            List<decimal> indexes = new List<decimal>();
+
+            if (totalWater % 2 == 0)
             {
-                consumedWater += bottlesCapacity - bottlesArray[i];
+                for (int i = 0; i < allBottles; i++)
+                {
+
+                    consumedWater += bottlesCapacity - bottlesArray[i];
+                    if (consumedWater <= totalWater)
+                        bottlesFilled++;
+                    else
+                        indexes.Add(i);
+                }
+                if (consumedWater <= totalWater)
+                {
+                    Console.WriteLine("Enough water!");
+                    Console.WriteLine("Water left: {0}l.", totalWater - consumedWater);
+                }
+
+                bottlesLeft = allBottles - bottlesFilled;
+
+                if (consumedWater > totalWater)
+                {
+                    Console.WriteLine("We need more water!");
+                    Console.WriteLine("Bottles left: {0}", bottlesLeft);
+                    Console.WriteLine("With indexes: {0}", string.Join(", ", indexes));
+                    Console.WriteLine("We need {0} more liters!", consumedWater - totalWater);
+                }
             }
-            if(consumedWater<=totalWater)
+            else
             {
-                Console.WriteLine("Enough water!");
-                Console.WriteLine("Water left: {0}l.", totalWater-consumedWater);
+                for (decimal i = allBottles - 1; i >= 0; i--)
+                {
+                    consumedWater += bottlesCapacity - bottlesArray[(int)i];
+                    if (consumedWater <= totalWater)
+                        bottlesFilled++;
+                    else
+                        indexes.Add(i);
+                }
+                if (consumedWater <= totalWater)
+                {
+                    Console.WriteLine("Enough water!");
+                    Console.WriteLine("Water left: {0}l.", totalWater - consumedWater);
+                }
+
+                bottlesLeft = allBottles - bottlesFilled;
+
+                if (consumedWater > totalWater)
+                {
+                    Console.WriteLine("We need more water!");
+                    Console.WriteLine("Bottles left: {0}", bottlesLeft);
+                    Console.WriteLine("With indexes: {0}", string.Join(", ", indexes));
+                    Console.WriteLine("We need {0} more liters!", consumedWater - totalWater);
+                }
             }
-
-
-
-
-
-
-
-           ////printing
-           //Console.WriteLine("Enough water!");
-           //Console.WriteLine("Water left: {0}l.");
-           //
-           //Console.WriteLine("We need more water!");
-           //Console.WriteLine("Bottles left: {0}");
-           //Console.WriteLine("With indexes: {0}");
-           //Console.WriteLine("We need {0} more liters!");
         }
     }
 }
